3f1d396166b9aa53c3c0a1ad5213454b06235b37
Huang Yonggang <huangyg@mars22.com>
2015-06-07 06:41:08 +0800

    8

--- learnyounode/8.js
+++ learnyounode/8.js
@@ -1,7 +1,11 @@
 
-var http = require('http'),bl = require('bl');
+var http = require('http');
+var bl = require('bl');
 
 http.get('http://www.pku.edu.cn',function (response){
+	response.on('end',function (e){
+		console.log("end..",e.toString())
+	});
 	response.pipe(bl(function (err,data){
 		console.log("\n\n\n\ndata:",data.toString())
 	}));
