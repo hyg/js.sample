d0bef73b47987f6bbcef516b303faa5e1025781f
Huang Yonggang <huangyg@mars22.com>
2015-06-09 08:27:26 +0800

    13

---
+++ learnyounode/13.js
@@ -0,0 +1,43 @@
+var http = require('http');
+var url = require('url');
+
+var server = http.createServer(function (req, res) {
+	if(req.method == 'GET') {
+		var purl = url.parse(req.url, true);
+		switch(purl.pathname){
+			case '/api/parsetime':
+				parsetime(purl,req, res);
+				break;
+			case '/api/unixtime':
+				unixtime(purl,req, res);
+				break;
+		}
+	}
+});
+
+server.listen(8000);
+
+function parsetime(purl,req, res){
+	var iso = new Date(purl.query.iso);
+	var obj = {hour:iso.getHours(),minute:iso.getMinutes(),second:iso.getSeconds()}
+	console.log(obj);
+	var body = JSON.stringify(obj);
+	console.log(body);
+	res.writeHead(200, { 'Content-Length': body.length,'Content-Type': 'application/json' })
+	
+	res.write(body,'utf8');
+	res.end();
+};
+
+function unixtime(purl,req, res){
+	var iso = new Date(purl.query.iso);
+	var obj = {unixtime:iso.getTime()}
+	console.log(obj);
+	var body = JSON.stringify(obj);
+	console.log(body);
+	res.writeHead(200, { 'Content-Length': body.length,'Content-Type': 'application/json' })
+	
+	res.write(body,'utf8');
+	res.end();
+};
+
