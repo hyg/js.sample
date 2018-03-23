7d0be8be91e89cbe60cbf7e46a719cba2fe3c56f
Huang Yonggang <huangyg@mars22.com>
2018-03-24 05:05:37 +0800

    letsX test
    先在peer事件里用http GET方法连一下各peer。
    估计是不行的。
    
    考虑穿透NAT，
    可以用k-rpc。
    
    或者设法拿到http服务的NAT外网端口。

--- dht/dht.js
+++ dht/dht.js
@@ -7,23 +7,34 @@ var str = "cognize" ;
 var infoHash = SHA1.hex(str);
 //infoHash = 'e3811b9539cacff680e418124272177c47477157' ;
 
-console.log("SHA1.hex("+str+")="+infoHash);
+console.log("SHA1.hex(\""+str+"\")="+infoHash);
 // SHA1.hex(str)=31d82293d8e05b4b6d4828e068d43ab2900a4b26
 // SHA1.b64(str)=Mdgik9jgW0ttSCjgaNQ6spAKSyY=
 
 
 var dht = new DHT()
 
-dht.announce(infoHash) ; //dhtjs
+dht.on('peer', function (peer, Hash, from) {
+  console.log('found potential peer ' + peer.host + ':' + peer.port + ' through ' + from.address + ':' + from.port + '\tHash : ' + Hash.toString('hex'));
 
-
-dht.listen(20000, function () {
-  console.log('now listening')
+  //var addr = dht.address() ;
+  //console.log('dht.address() return:\n' + addr.address + '\n' + addr.family + '\n' + addr.port) ;
+})
+//dht.on('node', function (node) { 
+  //console.log(' found a new node \t');
+  //console.log(node );
+//})
+dht.on('listening', function () { 
+  var addr = dht.address() ;
+  console.log('listening... dht.address() return:\n' + addr.address + '\n' + addr.family + '\n' + addr.port) ;
 })
 
-dht.on('peer', function (peer, infoHash, from) {
-  console.log('found potential peer ' + peer.host + ':' + peer.port + ' through ' + from.address + ':' + from.port)
+dht.on('ready', function () { 
+  var addr = dht.address() ;
+  console.log('ready... dht.address() return:\n' + addr.address + '\n' + addr.family + '\n' + addr.port) ;
 })
 
 // find peers for the given torrent info hash
-dht.lookup(infoHash)
+dht.lookup(infoHash);
+dht.announce(infoHash,34857) ; //dhtjs
+
---
+++ dht/letsX.js
@@ -0,0 +1,112 @@
+
+var Hashes = require('jshashes');
+var DHT = require('bittorrent-dht'); // see https://github.com/webtorrent/bittorrent-dht
+var fs = require('fs');
+var http = require('http');
+
+var url = require("url");
+var yaml = require('js-yaml');
+
+var SHA1 = new Hashes.SHA1;
+var str = "cognize" ;
+var infoHash = SHA1.hex(str);
+
+console.log("SHA1.hex(\""+str+"\")="+infoHash);
+// SHA1.hex(str)=31d82293d8e05b4b6d4828e068d43ab2900a4b26
+// SHA1.b64(str)=Mdgik9jgW0ttSCjgaNQ6spAKSyY=
+
+var dht = new DHT()
+
+dht.on('peer', function (peer, Hash, from) {
+  console.log('found potential peer ' + peer.host + ':' + peer.port + ' through ' + from.address + ':' + from.port + '\tHash : ' + Hash.toString('hex'));
+  var options = {
+	  hostname: peer.host,
+	  port: peer.port,
+	  method: 'GET',
+	  headers: {
+		'Content-Type': 'application/x-yaml'
+	  }
+	};
+  var req = http.request(options, function(res) {
+	  //console.log('STATUS: ' + res.statusCode);
+	  //console.log('HEADERS: ' + JSON.stringify(res.headers));
+	  res.setEncoding('utf8');
+
+	  res.on('data', function (chunk) {
+		console.log('response BODY: ' + chunk);
+	  });
+	});
+	
+	req.write('dht test');
+	req.end();
+})
+
+dht.on('ready', function () { 
+  dht.announce(infoHash,34857) ; //dhtjs
+  var addr = dht.address() ;
+  console.log('ready... dht.address() return:\n' + addr.address + '\n' + addr.family + '\n' + addr.port) ;
+})
+
+// find peers for the given torrent info hash
+dht.lookup(infoHash);
+//dht.announce(infoHash,34857) ; //dhtjs
+
+var server = http.createServer(function (req, res) {
+	var chunk = ""; 
+	req.on('data', function(data){
+      chunk += data ;
+    });
+    req.on('end', function(){
+        console.log('BODY: ' + chunk);
+        console.log('BODY length: ' + chunk.length);
+        console.log('method: ' + req.method);
+        const ip = res.socket.remoteAddress;
+        const port = res.socket.remotePort;
+        console.log(`你的IP地址是 ${ip}，你的源端口是 ${port}。`);
+        
+        var body = yaml.load(chunk);
+        if(req.method == 'POST') {
+            
+        } 
+		if(req.method == 'PUT') {
+		} 
+		if(req.method == 'GET') {
+			var pathname = url.parse(req.url).pathname;
+			var realPath = pathname.substring(1);
+			console.log(realPath);
+		}
+    })
+});
+server.listen(53879);  // letsX 
+
+
+function sent(item,method,callback){
+	var itemyaml = yaml.safeDump(item);
+	var options = {
+	  hostname: config.server.url,
+	  port: config.server.port,
+	  method: 'POST',
+	  headers: {
+		'Content-Type': 'application/x-yaml'
+	  }
+	};
+	
+	console.log("sending account to server...\n");
+
+	var req = http.request(options, function(res) {
+	  //console.log('STATUS: ' + res.statusCode);
+	  //console.log('HEADERS: ' + JSON.stringify(res.headers));
+	  res.setEncoding('utf8');
+
+	  res.on('data', function (chunk) {
+		console.log('response BODY: ' + chunk);
+		if (typeof(callback) != "undefined") {
+			callback(chunk);
+		}
+		emitter.emit("postsync");
+	  });
+	});
+	
+	req.write(itemyaml);
+	req.end();
+}
