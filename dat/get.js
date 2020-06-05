c5d60cf4268e924050f7052cded8baee4f55f02e
Huang Yonggang <huangyg@mars22.com>
2020-06-06 03:15:40 +0800

    path

--- dat/get.js
+++ dat/get.js
@@ -1,7 +1,7 @@
 var Dat = require('dat-node')
 
 // 1. Tell Dat where to download the files
-Dat('/download/PSMD', {
+Dat('download/PSMD', {
   // 2. Tell Dat what link I want
   key: 'adc78b18e36e0944e7dd7e77074ac083a49a0e837ddfbb1b0d43cbe06782f7a8' // (a 64 character hash from above)
 }, function (err, dat) {
