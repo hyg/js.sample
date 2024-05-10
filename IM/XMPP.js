9d41ea2fd0caf2b9a90d878a95630f0c55d27df9
Huang,Yonggang <huangyg@mars22.com>
2024-05-10 17:18:18 +0800

    - 07:45	[使用wechty做一个微信机器人范例](#20240510074500) - 09:30	[完成1609 COM metadata](#20240510093000) - 14:00	[整理今年夏季使用的NRV metadata](#20240510140000) - 14:30	[实现自定义DRI,NRV的代码](#20240510143000) - 16:00	[日小结时间统计增加一列todo，累计还没有进行的todo项时间。学习数组插入数组。](#20240510160000)

--- IM/XMPP.js
+++ IM/XMPP.js
@@ -1,5 +1,6 @@
 const { client, xml } = require("@xmpp/client");
 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
 const xmpp = client({
     service: "chat.jabb.im",
@@ -8,6 +9,9 @@ const xmpp = client({
     password: "123456",
 });
 
+xmpp.on("status", (status) => {
+    console.debug(status);
+});
 
 xmpp.on("error", (err) => {
     console.error(err);
@@ -18,31 +22,41 @@ xmpp.on("offline", () => {
 });
 
 xmpp.on("stanza", async (stanza) => {
-    console.log(stanza);
+    console.log(stanza.toString());
     if (stanza.is("message")) {
         //console.log(stanza);
-        const message = xml(
-            "message",
-            { type: "chat", to: "hyg@jabbim.com" },
-            xml("body", {}, "你好。"),
-        );
-        await xmpp.send(message);
+        console.log("stanza.body: "+stanza.children["body"]);
+        console.log("stanza.attrs.from: "+stanza.attrs.from);
+        const { to, from } = stanza.attrs;
+        xmpp.send(xml("message", { to: "hyg@jabbim.com", type: "chat" }, xml("body", null, "yes, i see.")));
+        /* 
+                const message = xml(
+                    "message",
+                    { type: "chat", to: "hyg@jabbim.com" },
+                    xml("body", {}, "你好。"),
+                );
+                await xmpp.send(message); */
         //await xmpp.stop();
     }
 });
 
 xmpp.on("online", async (address) => {
     // Makes itself available
-    console.log("online"+address);
+    console.log("online: " + address.toString());
     //await xmpp.send(xml("presence"));
 
     // Sends a chat message to itself
-    const message = xml(
+    /* const message = xml(
         "message",
         { type: "chat", to: "hyg@jabbim.com" },
         xml("body", {}, "hello world"),
     );
-    await xmpp.send(message);
+    await xmpp.send(message); */
+    const presence = xml('presence', {},
+        xml('show', {}, 'chat'),
+        xml('status', {}, 'presence!'),
+    )
+    xmpp.send(presence)
 });
 
 xmpp.start().catch(console.error);
