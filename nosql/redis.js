2d16227258a2eeec595cb2df6005510350d7c420
Huang,Yonggang <huangyg@mars22.com>
2024-05-23 17:06:34 +0800

    - 07:45	[新版个人模型的view draft](#20240523074500) - 14:00	[PSMD新合同的view draft](#20240523140000) - 16:00	[nosql](#20240523160000)

--- nosql/redis.js
+++ nosql/redis.js
@@ -4,13 +4,20 @@ const redisClient = redis.createClient() // 创建客户端
 
 // 监听错误信息
 redisClient.on('err', err => {
-  console.log('redis client error: ', err)
+    console.log('redis client error: ', err)
 })
 
 // 连接
-redisClient.connect(6379, '192.168.3.69').then(() => {
-  redisClient.set('name', 'PSMD')
-    .then(val => {
-      console.log(val)
-    })
+redisClient.connect(6379, '127.0.0.1').then(() => {
+    redisClient.set('name', 'huangyg')
+        .then(val => {
+            console.log("set", val)
+            redisClient.get('name').then(val => {
+                console.log("get", val)
+                redisClient.del('name').then(val => {
+                    console.log("del", val)
+                    redisClient.quit()
+                })
+            })
+        })
 })
