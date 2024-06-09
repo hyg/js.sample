e4dbde122f76b7a87e7366f0293e7777794b8df5
Huang,Yonggang <huangyg@mars22.com>
2024-06-09 17:37:29 +0800

    - 07:45	[entity draft](#20240609074500) - 09:30	[ego draft](#20240609093000) - 14:00	[https://www.npmjs.com/package/commander](#20240609140000) - 14:30	[选择有价值的数据显示到entry页面](#20240609143000) - 16:00	[linkml, yaml-ld](#20240609160000)

---
+++ commander/test.js
@@ -0,0 +1,13 @@
+const { program } = require('commander');
+
+program
+  .option('--first')
+  .option('-s, --separator <char>');
+
+program.parse();
+
+const options = program.opts();
+const limit = options.first ? 1 : undefined;
+console.log(program.args[0].split(options.separator, limit));
+
+const log = s => console.log(s);
