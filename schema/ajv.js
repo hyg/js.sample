f476e8ee5510d525ce37d2a800115c5226827c05
Huang,Yonggang <huangyg@mars22.com>
2024-07-03 17:24:00 +0800

    - 09:30	[day plan, day log, waiting list](#20240703093000) - 14:00	[寻找可编程的,去中心化的信息发布方式](#20240703140000) - 14:30	[了解免安装web im的机制](#20240703143000) - 16:00	[用yaml写json schema并且验证。](#20240703160000)

---
+++ schema/ajv.js
@@ -0,0 +1,141 @@
+const yaml = require('js-yaml');
+const Ajv = require("ajv")
+const ajv = new Ajv() // options can be passed, e.g. {allErrors: true}
+
+const schema = {
+  type: "object",
+  properties: {
+    foo: {type: "integer"},
+    bar: {type: "string"}
+  },
+  required: ["foo"],
+  additionalProperties: false
+}
+
+const yamlschemastr = `
+$schema: http://json-schema.org/draft-07/schema#
+title: Entity
+type: object
+properties:
+  id:
+    type: string
+  name:
+    type: string
+  aliases: 
+    type: array
+    items:
+      type: string
+  data:
+    type: object
+    properties:
+      id:
+        type: string
+      name:
+        type: string
+      entityname:
+        type: string
+      events:
+        type: array
+        items:
+          type: object
+          properties:
+            id: 
+              type: string
+            name: 
+              type: string
+            readme: 
+              type: string
+            log: 
+              type: string
+            comment: 
+              type: string
+      todos:
+        type: array
+        items:
+          type: object
+          properties:
+            id: 
+                type: string
+            name: 
+                type: string
+            readme: 
+                type: string
+            log: 
+                type: string
+            comment: 
+                type: string
+  cognize:
+    type: string
+required:
+  - id
+  - name
+`;
+
+var validate = ajv.compile(schema)
+
+const data = {
+  foo: 1,
+  bar: "abc"
+}
+
+var data2 ;
+
+var datastr = `
+id: huangyg
+name: huang yonggang
+aliases: 
+  - ego
+data:
+  id: hyg.data
+  name: data instance of entity "huangyg"
+  entityid: huangyg
+  events:
+    - id: hyg.data.event
+      name: the event queue of entity "huangyg"
+      readme: "ego.data.event"
+  todos: 
+    - id: hyg.data.todo
+      name: the todo item of entity "huangyg"
+      readme: "ego.data.todo"
+cognize: "ego.data.congnize"
+`;
+
+const valid = validate(data)
+if (!valid) console.log(validate.errors)
+
+var yamlschema = yaml.load(yamlschemastr);
+
+data2 = yaml.load(datastr);
+var result = ajv.validate(yamlschema,data2);
+if (!result) console.log(ajv.errors);
+
+datastr = `
+id: ORCID:0009-0005-7296-5316
+name: 黄勇刚
+aliases:
+  - huangyg
+  - mars22
+  - fromoon
+  - samadhi
+data:
+  entityid: ORCID:0009-0005-7296-5316
+  todos:
+    - id: "20240612165400"
+      name: linkml, yaml-ld
+      subject: learn
+      time: 60
+      readme: |
+        - https://linkml.io/linkml/schemas/models.html
+        - https://linkml.io/linkml/intro/tutorial03.html
+cognize: "1"
+protocol: 2
+offer: 3`;
+
+data2 = yaml.load(datastr);
+
+result = ajv.validate(yamlschema,data2);
+if (!result) console.log(ajv.errors);
+
+validate = ajv.compile(yamlschema);
+result = validate(data2);
+if (!result) console.log(ajv.errors);
