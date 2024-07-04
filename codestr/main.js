69591d452b6566b145db3305a24fe61899180c79
黄勇刚 <huangyg@mars22.com>
2024-07-04 17:10:54 +0800

    - 09:30	[day plan, day log, waiting list](#20240704093000) - 14:00	[了解免安装web im的机制：gitter.im](#20240704140000) - 14:30	[github project](#20240704143000) - 16:00	[metadata中自带处理代码的范例](#20240704160000)

--- codestr/main.js
+++ codestr/main.js
@@ -2,9 +2,18 @@ var fs = require('fs');
 var yaml = require('js-yaml');
 
 var dataobj = yaml.load(fs.readFileSync("test.yaml"));
-var codestr =  dataobj.code ;
-import(codestr).then((code)=>{
+
+var mjsstr =  dataobj.mjs ;
+import(mjsstr).then((code)=>{
     code.datatoview(dataobj);
 }).catch((err) => {
     console.log("error:"+err);
 });
+
+var jsstr =  dataobj.js ;
+try{
+    require(jsstr).datatoview(dataobj);
+}
+catch(err){
+    console.log("error:"+err);
+};
--- codestr/test.mjs
+++ codestr/test.mjs
@@ -1,6 +1,3 @@
-
-
-
 export function datatoview(obj) {
     console.log("data:" + obj.data);
     console.log("60:" + obj.item[60]);
