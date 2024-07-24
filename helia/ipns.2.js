bd4a0e93297d054db48cbe741ee0f6c7c8c40525
Huang Yonggang <huangyg@xuemen.com>
2024-07-24 15:52:14 +0800

    helia ipns debuf

--- helia/ipns.2.js
+++ helia/ipns.2.js
@@ -2,11 +2,13 @@ import { createHelia } from 'helia'
 import { ipns } from '@helia/ipns'
 import { unixfs } from '@helia/unixfs'
 import { CID } from 'multiformats/cid'
+import { json } from '@helia/json'
 
 const helia = await createHelia()
 console.log("helia:",helia)
 
 // store some data to publish
+/*
 const fs = unixfs(helia)
 console.log("fs:",fs)
 
@@ -15,14 +17,27 @@ const cid = CID.parse("bafkreiaixnpf23vkyecj5xqispjq5ubcwgsntnnurw2bjby7khe4wnji
 for await (const buf of fs.cat(cid)) {
   console.info('cat bu cid:',buf)
 }
+*/
 
 const name = ipns(helia)
 console.log("name:",name)
-const peerId = CID.parse("QmP5zqdzNMXxs2eSzBZXCTLLYHgvjMBpNM33sxhg6dYcTm")
+const j = json(helia)
+console.log("j:",j)
+
+//const cid = CID.parse("bafzbeiedgxvd4c6sudmqrwoswe5kc3dqlopkqmcyidzosq6nntdqejwv7e")
+//const peerId = await j.get(cid)
+//const peerId = "QmbXWLrgz3rciF9rqGf59i6iKPmy3fdPNSFXTiYhYAK66W"
+const peerId = await helia.libp2p.services.keychain.exportPeerId("my-key")
 console.log("peerId:",peerId)
+
 const result = await name.resolve(peerId)
 console.log("result:",result)
+
+/*
 for await (const buf of fs.cat(result.cid)) {
   console.info('cat bu result.cid:',buf)
 }
+*/
 
+const obj = await j.get(result.cid)
+console.log("get by result.cid:",obj)
--- helia/ipns.js
+++ helia/ipns.js
@@ -1,9 +1,10 @@
 import { createHelia } from 'helia'
 import { ipns } from '@helia/ipns'
 import { unixfs } from '@helia/unixfs'
+import { json } from '@helia/json'
 
 const helia = await createHelia()
-console.log("helia:",helia)
+//console.log("helia:",helia)
 //const start = await helia.start()
 //console.log("start:",start)
 
@@ -15,16 +16,33 @@ const keyInfo = await helia.libp2p.services.keychain.createKey('my-key','rsa')
 const peerId = await helia.libp2p.services.keychain.exportPeerId(keyInfo.name)
 console.log("keyInfo:",keyInfo)
 console.log("peerId:",peerId)
+console.log("peerId.multihash:",peerId.multihash)
+console.log("peerId.privateKey:",peerId.privateKey)
+console.log("peerId.publicKey:",peerId.publicKey)
+console.log("peerId.type:",peerId.type)
+console.log("peerId.toBytes():",peerId.toBytes())
+console.log("peerId.toCID():",peerId.toCID())
+console.log("peerId.toString():",peerId.toString())
 
 // store some data to publish
-const fs = unixfs(helia)
-console.log("fs:",fs)
+//const fs = unixfs(helia)
+//console.log("fs:",fs)
 
-const emptyDirCid = await fs.addDirectory()
-console.log("emptyDirCid:",emptyDirCid)
+//const emptyDirCid = await fs.addDirectory()
+//console.log("emptyDirCid:",emptyDirCid)
 
-const cid = await fs.addBytes(Uint8Array.from([0, 1, 2, 3, 4]))
+//const cid = await fs.addBytes(Uint8Array.from([0, 1, 2, 3, 4]))
+//console.log("cid:",cid)
+
+const j = json(helia)
+
+const cid = await j.add({
+  hello: 'world'
+})
 console.log("cid:",cid)
+
+//const obj = await j.get(cid)
+//console.info(obj)
 
 // publish the name
 await name.publish(peerId, cid)
@@ -32,12 +50,16 @@ await name.publish(peerId, cid)
 // resolve the name
 const result = await name.resolve(peerId)
 console.log("result:",result)
-console.info(result.cid, result.path)
+//console.info(result.cid, result.path)
+const obj = await j.get(result.cid)
+console.info("get by result.cid:",obj)
 
+/*
 for await (const buf of fs.cat(cid)) {
-  console.info('cat bu cid:',buf)
+  console.info('cat by cid:',buf)
 }
 
 for await (const buf of fs.cat(result.cid)) {
   console.info('cat by result.cid:',buf)
 }
+*/
