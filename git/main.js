1801ae3de5df498cac35193bff83948f47d37576
hyg <huangyg@mars22.com>
2025-03-12 17:58:47 +0800

    test autocrlf

--- git/main.js
+++ git/main.js
@@ -8,14 +8,28 @@ const { exec } = require('node:child_process');
 const log = s => console.log(s);
 
 const add = exec('git add .');
+dayover();
 
-let sha = git.commit({
-    fs,
-    dir: '.',
-    author: {
-        name: 'huang yonggang',
-        email: 'huangyg@mars22.com',
-    },
-    message: 'git in nodejs sample\n will be used in day over operations.\njuse test for now.'
-});
-log(sha);
+async function dayover(){
+
+    let sha = await git.commit({
+        fs,
+        dir: "D:\\huangyg\\git\\raw",
+        author: {
+            name: 'huang yonggang',
+            email: 'huangyg@mars22.com',
+        },
+        message: 'git in nodejs sample\n will be used in day over operations.\njuse test for now.'
+    });
+    log(sha);
+    
+    let pushResult = await git.push({
+        fs,
+        http,
+        dir: "D:\\huangyg\\git\\raw",
+        remote: 'all',
+        ref: 'master',
+        onAuth: () => ({ username: process.env.GITHUB_TOKEN }),
+      })
+      console.log(pushResult)
+}
---
+++ git/simple-git.js
@@ -0,0 +1,76 @@
+const simpleGit = require('simple-git');
+const GIT_SSH_COMMAND = 'C:/Progra~1/PuTTY/plink.exe';
+
+async function status(workingDir) {
+   let statusSummary = null;
+   try {
+      statusSummary = await simpleGit(workingDir).status();
+   } catch (e) {
+      // handle the error
+   }
+   return statusSummary;
+}
+
+// using the async function
+//status("D:\\huangyg\\git\\raw").then((status) => console.log(status));
+
+var commitmsg = `- 10:00~13:59	js: [git sample](#20250312100000)
+- 14:00~14:29	raw: [复习五元庄第四式](#20250312140000)
+- 14:30~14:59	raw: [复习五元庄第五式](#20250312143000)
+- 16:00~16:59	learn: [graphsync protocol](#20250312160000)`;
+gitstep("D:\\huangyg\\git\\blog",commitmsg);
+gitstep("D:\\huangyg\\git\\draft",commitmsg);
+gitstep("D:\\huangyg\\git\\ego",commitmsg);
+gitstep("D:\\huangyg\\git\\js.sample",commitmsg);
+
+async function gitstep(path,msg){
+      let statusSummary = null;
+   try {
+      statusSummary = await simpleGit(path).status();
+   } catch (e) {
+      // handle the error
+   }
+   if(statusSummary.files != []){
+      console.log("file changed:",statusSummary.files);
+      simpleGit(path,{autocrlf:false})
+      .env('GIT_SSH_COMMAND', GIT_SSH_COMMAND)
+      .add('./*')
+      .commit(msg)
+      .push('all', 'master')
+      .then((data) => {
+         console.log('success:',path,"\n",data);
+      })
+      .catch((err) => {
+          console.log(err);
+      });
+   }else{
+      console.log("non file changed:",path);
+   }
+}
+
+/* simpleGit("D:\\huangyg\\git\\raw")
+   .env('GIT_SSH_COMMAND', GIT_SSH_COMMAND)
+   .add('./*')
+   .commit('morning')
+   .push('all', 'master')
+   .then((data) => {
+      console.log('raw success:',data);
+   })
+   .catch((err) => {
+       console.log(err);
+   });; */
+
+/* simpleGit("D:\\huangyg\\git\\blog")
+   .env('GIT_SSH_COMMAND', GIT_SSH_COMMAND)
+   .add('./*')
+   .commit(`- 10:00~13:59	js: [git sample](#20250312100000)
+- 14:00~14:29	raw: [复习五元庄第四式](#20250312140000)
+- 14:30~14:59	raw: [复习五元庄第五式](#20250312143000)
+- 16:00~16:59	learn: [graphsync protocol](#20250312160000)`)
+   .push('all', 'master')
+   .then((data) => {
+      console.log('blog success:',data);
+   })
+   .catch((err) => {
+       console.log(err);
+   });; */
