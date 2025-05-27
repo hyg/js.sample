d982b36bcbde88afaeef5edd75b6c25a9b2f3a22
Huang,Yonggang <huangyg@mars22.com>
2025-05-27 18:22:52 +0800

    - 16:00~16:59	learn: [量子物理的数学基础](#20250527160000)

---
+++ mysql/redahomes.js
@@ -0,0 +1,30 @@
+var mysql      = require('mysql');
+require('dotenv').config()
+
+var connectionSchool = mysql.createConnection({
+  host     : process.env.DB_SCHOOLS_HOST,
+  user     : process.env.DB_SCHOOLS_USER,
+  password : process.env.DB_SCHOOLS_PASSWORD,
+  database : process.env.DB_SCHOOLS_NAME
+});
+
+var connection = mysql.createConnection({
+  host     : process.env.DB_HOST,
+  user     : process.env.DB_USER,
+  password : process.env.DB_PASSWORD,
+  database : process.env.DB_NAME
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  } 
+  console.log('connected as id ' + connection.threadId);
+});
+
+connection.query('SELECT table_name FROM information_schema.TABLES ', function (error, results, fields) {
+  if (error) throw error;
+  console.log('tables: ', results);
+});
+connection.end();
