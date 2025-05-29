f0de7b0a3332cbc6265535a18d8c00027706f3c7
Huang,Yonggang <huangyg@mars22.com>
2025-05-29 21:56:34 +0800

    - 14:00~16:59	raw: [2025.6.食材预算](#20250529140000) - 14:30~15:59	js: [json-query](#20250529143000)

---
+++ query/json-query.js
@@ -0,0 +1,36 @@
+var jsonQuery = require('json-query');
+var fs = require('fs');
+var yaml = require('js-yaml');
+
+function log(...s) {
+    s[0] = log.caller.name + "> " + s[0];
+    console.log(...s);
+}
+
+function loadAER(year) {
+    var AERmap = new Object();
+    var voucherfolder = "../../ego/data/voucher/" + year;
+    fs.readdirSync(voucherfolder).forEach(file => {
+        if (file.substr(0, 4) == "AER.") {
+            var AER = yaml.load(fs.readFileSync(voucherfolder + "/" + file, 'utf8'));
+            AERmap[file] = AER;
+        }
+    });
+    return AERmap;
+}
+var date1= new Date("2025-05-01");
+var date2= new Date("2025-05-10");
+var helpers = {
+    period: function(input){
+        //log(yaml.dump(input));
+        var date = Date.parse(input.date);
+      if (date > date1 & date < date2){
+        return input
+      }
+    }
+}
+var AERmap = loadAER(2025);
+//log(yaml.dump(AERmap));
+
+var result = jsonQuery('[**][*:period]', {data: AERmap, locals: helpers}).value;
+log(yaml.dump(result));
