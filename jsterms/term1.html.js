f9f3e513fe2ecf5f837659092fa9d62b6a0d76a7
Huang,Yonggang <huangyg@mars22.com>
2025-07-04 20:49:30 +0800

    - 14:00~15:29	PSMD: [整理default模型](#20250704140000) - 16:00~16:59	infra: [范例--利用js模块组合实现合同条款的组合。](#20250704160000)

---
+++ jsterms/term1.html.js
@@ -0,0 +1,61 @@
+
+var metadata = {
+        id: "111111",
+        name: "term1",
+        readme: "term1 readme"};
+var text =  "term1 text";
+
+var env = {
+        	rule: ["r0","r1","r2"],
+        	event: ["e0","e1","e2"],
+        	state: ["s0","s1","s2"],
+        	action: ["a0","a1","a2"],
+        	asset: []
+        };
+
+var state = "s0";
+var code = {
+        	e0: function(){
+                console.log("enter e0. state=",state);
+        		 state = "s0";	
+        	},
+        	e1: function(){
+        		console.log("enter e1. state=",state);
+        		 state = "s1";
+        		//console.log("%o",this);
+        		 a1("r0");
+        	},
+        	e2: function(){
+                console.log("enter e2. state=",state);
+        		var ret ;
+        		switch( state){
+        			case "s0":
+        				 state = "s1";
+        				 a0("r0");
+        				 a2("r1");
+        				break;
+        			case "s1":
+        			      state = "s2";
+        			      a1("r0");
+        			      a0("r1");
+        			     break;
+        			case "s2":
+        				 state = "s0";
+        				 a0("r0");
+        				 a1("r1");
+        			    break;
+        		}
+        		return ret;
+        	}
+        };
+
+        	function a0(rule){
+        		console.log(rule+": action a0.");
+       		};
+            
+        	function a1(rule){
+           		console.log(rule+": action a1.");
+           	};
+	        function a2(rule){
+	        	console.log(rule+": action a2.");
+           	};
