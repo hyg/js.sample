46caf747246307db4a88978f81238a0ccbecba77
huang yonggang <huangyg@mars22.com>
2025-07-23 22:58:53 +0800

    BOOTSTRAPS

--- dht/p2p.mjs
+++ dht/p2p.mjs
@@ -4,7 +4,7 @@ import dgram from 'dgram';
 const socket = dgram.createSocket('udp4');
 socket.bind(30000, () => {
     console.log('Socket is listening on port 30000');
-  });
+});
 // 监听端口
 socket.on('listening', function () {
     console.log('socket listening event.');
@@ -57,15 +57,37 @@ const dht = new DHT();   // 让 DHT 复用 socket
 //const dht = new DHT({ socket });   // 让 DHT 复用 socket
 //console.log("socket: %O",socket);
 //console.log("dht: %O",dht);
-console.log("before addnode(), nodes:",dht.toJSON().nodes)
+console.log("before addnode(), nodes:", dht.toJSON().nodes)
 //dht.addNode("router.bittorrent.com",6881);
-dht.addNode({host:"router.bittorrent.com",port:6881});
+/* dht.addNode({host:"router.bittorrent.com",port:6881});
 dht.addNode({host:"dht.transmissionbt.com",port:6881});
 dht.addNode({host:"router.utorrent.com",port:6881});
 dht.addNode({host:"ns-1.x-fins.com",port:6969});
 dht.addNode({host:"tracker.vanitycore.co",port:6881});
 console.log("after addnode(), nodes:",dht.toJSON().nodes);
-
+ */
+const BOOTSTRAPS = [
+    { host: '34.197.35.250', port: 6880 },
+    { host: '72.46.58.63', port: 51413 },
+    { host: '46.53.251.68', port: 16970 },
+    { host: '191.95.16.229', port: 55998 },
+    { host: '79.173.94.111', port: 1438 },
+    { host: '45.233.86.50', port: 61995 },
+    { host: '178.162.174.28', port: 28013 },
+    { host: '178.162.174.240', port: 28006 },
+    { host: '72.21.17.101', port: 22643 },
+    { host: '31.181.42.46', port: 22566 },
+    { host: '67.213.106.46', port: 61956 },
+    { host: '201.131.172.249', port: 53567 },
+    { host: '185.203.152.184', port: 2003 },
+    { host: '68.146.23.207', port: 42107 },
+    { host: '51.195.222.183', port: 8653 },
+    { host: '85.17.170.48', port: 28005 },
+    { host: '87.98.162.88', port: 6881 },
+    { host: '185.145.245.121', port: 8656 },
+    { host: '52.201.45.189', port: 6880 }
+];
+BOOTSTRAPS.forEach(([host, port]) => dht.addNode(host, port));
 dht.listen(20000, function () {
     console.log('dht listening 20000')
 })
@@ -75,11 +97,12 @@ dht.on('message', handleAppMessage);
 dht.on('peer', function (peer, infoHash, from) {
     console.log('found potential peer ' + infoHash + peer.host + ':' + peer.port + ' through ' + from.address + ':' + from.port);
     var strmsg = "笔记本发现你了。";
-    socket.send(strmsg, 0, strmsg.length, peer.port, peer.host );
+    socket.send(strmsg, 0, strmsg.length, peer.port, peer.host);
 })
 
 dht.on('node', function (node) {
     console.log('found node: %O ', node);
+    console.log("nodes:", dht.toJSON().nodes);
 })
 
 dht.on('warning', function (err) {
@@ -92,7 +115,14 @@ dht.on('error', function (err) {
 
 dht.on('ready', function () {
     console.log('ready');
-    console.log("nodes:",dht.toJSON().nodes)
+    console.log("nodes:", dht.toJSON().nodes);
+    dht.addNode({ host: "router.bittorrent.com", port: 6881 });
+    dht.addNode({ host: "dht.transmissionbt.com", port: 6881 });
+    dht.addNode({ host: "router.utorrent.com", port: 6881 });
+    dht.addNode({ host: "ns-1.x-fins.com", port: 6969 });
+    dht.addNode({ host: "tracker.vanitycore.co", port: 6881 });
+    console.log("after addnode(), nodes:", dht.toJSON().nodes);
+
 })
 
 var secretHash = "58c5d8483c4e7d19b86d1351d6cf89b9ae232400";
@@ -100,18 +130,26 @@ var secretHash = "58c5d8483c4e7d19b86d1351d6cf89b9ae232400";
 const INTERVAL_ANNOUNCE = 30 * 1000;
 const INTERVAL_LOOKUP = 20 * 1000;
 
-setInterval(() => dht.announce(secretHash,(err)=>{
-    if (err) return console.error("announce",err);
+setInterval(() => dht.announce(secretHash, (err) => {
+    if (err && err.message.includes('No nodes to query')) {
+        // 重新 bootstrap 并延迟重试
+        BOOTSTRAPS.forEach(([h, p]) => dht.addNode(h, p));
+      } else if (err) {
+        console.error('announce error:', err);
+      } else {
+        console.log('announce ok');
+      }
 }), INTERVAL_ANNOUNCE);
-/* setInterval(() => dht.lookup(secretHash, (err, peers) => {
+setInterval(() => dht.lookup(secretHash, (err, peers) => {
     if (err) return console.error(err);
     console.log('发现 peer: %s , %O', typeof peers, peers);
     //peers.forEach(p => console.log('peer ->', p.host, p.port));
     // peers = [{ host, port }, ...]
-}), INTERVAL_LOOKUP); */
+}), INTERVAL_LOOKUP);
 
 function handleAppMessage(msg, rinfo) {
-    if(!msg.toString().startsWith("笔记本:")){
+    console.log("messge event:", msg, rinfo);
+    if (!msg.toString().startsWith("笔记本:")) {
         var strmsg = "笔记本:" + msg;
         socket.send(strmsg, 0, strmsg.length, rinfo.port, rinfo.address); //将接收到的消息返回给客户端
     }
