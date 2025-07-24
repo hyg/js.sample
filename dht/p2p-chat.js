29ad114c667745dbd5f59b5cf863f09b91e1a458
huang yonggang <huangyg@mars22.com>
2025-07-24 17:32:52 +0800

    bootstrap peer event

--- dht/p2p-chat.js
+++ dht/p2p-chat.js
@@ -27,7 +27,31 @@ stun.request(STUN, { socket }, (err, res) => {
   console.log('[STUN] public', `${address}:${port}`);
 
   // 4. 连 DHT
-  const dht = new DHT();
+
+  const BOOTSTRAPS = [
+    { host: '34.197.35.250', port: 6880 },
+    { host: '72.46.58.63', port: 51413 },
+    { host: '46.53.251.68', port: 16970 },
+    { host: '191.95.16.229', port: 55998 },
+    { host: '79.173.94.111', port: 1438 },
+    { host: '45.233.86.50', port: 61995 },
+    { host: '178.162.174.28', port: 28013 },
+    { host: '178.162.174.240', port: 28006 },
+    { host: '72.21.17.101', port: 22643 },
+    { host: '31.181.42.46', port: 22566 },
+    { host: '67.213.106.46', port: 61956 },
+    { host: '201.131.172.249', port: 53567 },
+    { host: '185.203.152.184', port: 2003 },
+    { host: '68.146.23.207', port: 42107 },
+    { host: '51.195.222.183', port: 8653 },
+    { host: '85.17.170.48', port: 28005 },
+    { host: '87.98.162.88', port: 6881 },
+    { host: '185.145.245.121', port: 8656 },
+    { host: '52.201.45.189', port: 6880 }
+  ];
+
+  const dht = new DHT({ bootstrap: BOOTSTRAPS });   // 让 DHT 复用 socket
+  //const dht = new DHT();
   dht.listen();                 // 随机端口
   dht.on('ready', () => {
     console.log('[DHT] ready, node id', dht.nodeId.toString('hex'));
@@ -37,15 +61,13 @@ stun.request(STUN, { socket }, (err, res) => {
     });
   });
 
-  // 5. 发现对方
-  dht.on('peer', (_addr) => {
-    const [peerIP, peerPort] = _addr.split(':');
-    if (peerIP === address && Number(peerPort) === port) return; // 自己
-    console.log('[PEER] got', _addr);
-    // 6. 打洞：持续发心跳
-    const HEART = Buffer.from('💗');
-    setInterval(() => socket.send(HEART, 0, HEART.length, peerPort, peerIP), 1000);
-  });
+  // 5. 发现对方// 新代码：
+dht.on('peer', ({ host: peerIP, port: peerPort }) => {
+  if (peerIP === address && peerPort === port) return; // 自己
+  console.log('[PEER] got', `${peerIP}:${peerPort}`);
+  const HEART = Buffer.from('💗');
+  setInterval(() => socket.send(HEART, 0, HEART.length, peerPort, peerIP), 1000);
+});
 
   // 7. 接收数据
   socket.on('message', (msg, rinfo) => {
